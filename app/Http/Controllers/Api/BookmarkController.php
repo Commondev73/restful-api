a99be664b702b75200fa41990999f0bfae6ed6be
Darkness <----->
2019-11-23 08:54:01 +0700

    Bookmark&edit-Announcse&addRoute

--- app/Http/Controllers/Api/BookmarkController.php
+++ app/Http/Controllers/Api/BookmarkController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\PublicAnnouncesController;
 use Illuminate\Http\Request;
 use App\Models\Bookmark;
 use App\Models\Announces;
 use App\Models\Image_announces;
-use App\Http\Controllers\Api\PublicAnnouncesController;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Exception;
@@ -21,15 +21,26 @@ class BookmarkController extends Controller
         $this->id_user = auth()->user()->id;
     }
 
+    // public function index(PublicAnnouncesController $Announces)
+    // {
+    //     $bookmark = Bookmark::where('id_user', $this->id_user)->get();
+    //     foreach($bookmark as $data){
+    //         $list[] = $Announces->announcesByID($data->id_announces);
+    //     }
+    //     return response()->json($list, 200);
+    // }
+
     public function index()
     {
         $bookmark = Bookmark::where('id_user', $this->id_user)->get();
-        $PublicAnnounces = new PublicAnnounces();
-        foreach ($bookmark as $data) {
-            $announces_id = $PublicAnnounces->announcesByID($data->id_announces);
-            return response()->json($announces_id, 200);
-         }
-        // return response()->json($bookmark, 200);
+        foreach ($bookmark as $dataID) {
+            $id[] = $dataID->id_announces;
+        }
+        $announces = Announces::find($id);
+        foreach ($announces as $data) {
+            $data->image =  Image_announces::where('announcement_id', $data->id)->get();
+        }
+        return response()->json($announces, 200);
     }
 
 
@@ -41,22 +52,21 @@ class BookmarkController extends Controller
 
     public function store(Request $request)
     {
-        $rules= [
-            'id_user'=> 'required',
-            'id_announces'=>'required'
+        $rules = [
+            'id_announces' => 'required'
         ];
 
-        $validator = Validator::make($request->all(),$rules);
-        if($validator->fails()){
-            return response()->json($validator->errors(),400);
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
         }
 
         $bookmark = new Bookmark;
-        $bookmark->id_user = $request->id_user;
+        $bookmark->id_user = $this->id_user;
         $bookmark->id_announces = $request->id_announces;
         $bookmark->save();
 
-        return respones()->json($bookmark,201);
+        return response()->json($bookmark, 201);
     }
 
 
@@ -80,8 +90,8 @@ class BookmarkController extends Controller
 
     public function destroy($id)
     {
-        $bookmark = Bookmark::find($id);
-        $bookmark->delete();
+        $bookmark = Bookmark::where('id_user', $this->id_user)
+            ->where('id_announces', $id)->delete();
         return response()->json(null, 204);
     }
 }
